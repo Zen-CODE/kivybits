0bc79544e6ee8567d57c4162a1c2623036cc8746
Zen-CODE <zenkey.zencode@gmail.com>
2013-09-28 04:42:07 +0200

    Added Kivy property

--- PyCon/ZA 2013/code/properties_kivy.py
+++ PyCon/ZA 2013/code/properties_kivy.py
@@ -7,6 +7,10 @@ from kivy.event import EventDispatcher
 class KivyProperties(EventDispatcher):
     text = StringProperty()
 
+    # To listen for changes
+    def on_text(self, instance, value):
+        print "Changing to ", value
+
 if __name__ == '__main__':
     obj = KivyProperties()
     obj.text = "Me"
