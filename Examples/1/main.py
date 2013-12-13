54962a64dd5460d8d99d42949bc036cfb5001d5f
richard <zenkey.zencode@gmail.com>
2013-12-13 10:53:31 +0200

    Added logger, comments, and suggested but non-functional code

--- Examples/1/main.py
+++ Examples/1/main.py
@@ -4,40 +4,45 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.vkeyboard import VKeyboard
 from kivy.core.window import Window
 from kivy.properties import ObjectProperty
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 from functools import partial
 from kivy.uix.button import  Button
 from kivy.config import Config
-
+from kivy.logger import Logger
 
 Config.set('kivy', 'keyboard_mode', 'dock')
 
 
 class KeyboardListener(object):
     """
-    Handles the management of keyboard bindings and callbacks
+    This class handles the management of keyboard bindings and callbacks.
+    It also exposes a list of available keyboard layouts.
     """
     layouts = []  # List of available keyboard layouts
     _keyboard = None  # Current keyboard layout
     _callback = None  # Current callback
 
     def __init__(self):
-        """ Build a list of available keyboard layouts """
+        """ Build a list of available keyboard layouts."""
         super(KeyboardListener, self).__init__()
         self.layouts = [key for key in VKeyboard().available_layouts.keys()]
-        self.layouts.append('numeric.json')
+        #self.layouts.append('numeric.json')
 
-    def set_callback(self, callback, text_input, layout):
+    def set_callback(self, callback, layout):
         """
-        Set the function to be called when key presses occur
+        Set the function to be called when key presses occur.
         """
-        #Window.release_all_keyboards()
+        Window.release_all_keyboards()
         self._keyboard = Window.request_keyboard(
             self._keyboard_close,
-            text_input)
+            self)
 
         #if self._keyboard.widget:
+        Logger.info("main.py: self._keyboard.widget=" +
+                    str(self._keyboard.widget))
+        #if self._keyboard.widget:
+            #vkeyboard = self._keyboard.widget
+            #vkeyboard.layout = layout
+            #vkeyboard.bind(on_key_down=callback)
         if self._keyboard:
             self._keyboard.layout = layout
             self._keyboard.bind(on_key_down=callback)
@@ -46,7 +51,7 @@ class KeyboardListener(object):
     def _keyboard_close(self):
         """The keyboard has been closed. Clean up"""
         if self._callback:
-            print "Keyboard closed..."
+            Logger.info("main.py: Keyboard closed...")
             self._keyboard.unbind(on_key_down=self._callback)
             self._keyboard = None
             self._callback = None
@@ -62,7 +67,7 @@ Builder.load_string(
     orientation: 'vertical'
     Label:
         size_hint_y: 0.125
-        text: "I'm glad to see you Dave."
+        text: "Please select the keyboard layout."
     BoxLayout:
         id: kb_container
         size_hint_y: 0.125
@@ -102,13 +107,12 @@ class KeyboardTest(BoxLayout):
 
     def on_button_release(self, instance, layout):
         """ The button has been pressed """
-        print "Setting keyboard listed to ", layout
-        self._kb_listener.set_callback(self.on_key_press, instance, layout)
+        Logger.info("main.py: Setting keyboard listed to " + layout)
+        self._kb_listener.set_callback(self.on_key_press, layout)
 
     def on_key_press(self, *largs):
         """ A key has been pressed. Display it."""
         self.display_label.text = '[b]Key pressed[/b]\n' + largs[2]
-        return True
 
 
 class test(App):
