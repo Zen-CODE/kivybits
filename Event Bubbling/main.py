f5ebb4c847f19d203d3ab33ccb2e934f1eae6ef9
Zen-CODE <zenkey.zencode@gmail.com>
2014-02-26 15:50:20 +0200

    added alternative python UI creation

--- Event Bubbling/main.py
+++ Event Bubbling/main.py
@@ -2,7 +2,10 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 
-Builder.load_string('''
+use_kv = False
+
+if use_kv:
+    Builder.load_string('''
 <EventBubbler>:
     orientation: 'vertical'
     Label:
@@ -20,12 +23,41 @@ Builder.load_string('''
         Label:
             text: '4'
             on_touch_down: root.printme("label 4 on_touch_up")
-''')
+    ''')
 
 
 class EventBubbler(BoxLayout):
-    def printme(self, msg):
+
+    @staticmethod
+    def printme(msg):
         print msg
+
+    if not use_kv:
+        def __init__(self, **kwargs):
+            from kivy.uix.label import Label
+            from kivy.uix.boxlayout import BoxLayout
+
+            super(EventBubbler, self).__init__(**kwargs)
+            self.orientation = 'vertical'
+            self.add_widget(
+                Label(
+                    text="1",
+                    on_touch_down=lambda btn, tch: self.printme("1abel 1")))
+            self.add_widget(
+                Label(
+                    text="2",
+                    on_touch_down=lambda btn, tch: self.printme("1abel 2")))
+            box = BoxLayout(
+                on_touch_down=lambda box, tch: self.printme("box"))
+            box.add_widget(
+                Label(
+                    text="3",
+                    on_touch_down=lambda btn, tch: self.printme("1abel 3")))
+            box.add_widget(
+                Label(
+                    text="4",
+                    on_touch_down=lambda btn, tch: self.printme("1abel 4")))
+            self.add_widget(box)
 
 
 class BubbleApp(App):
