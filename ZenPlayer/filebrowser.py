a2347b85331fd5fc2d434715b9fb0a8235611644
Zen-CODE <zenkey.zencode@gmail.com>
2014-03-23 09:45:53 +0200

    Added reference and status print for filechooser

--- ZenPlayer/filebrowser.py
+++ ZenPlayer/filebrowser.py
@@ -3,15 +3,26 @@ Displays the file browsing screen for ZenPlayer
 """
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 
 
 Builder.load_string('''
 <ZenFileBrowser>:
+    filechooser: filechooser
+
     BoxLayout:
         orientation: 'vertical'
-        Label:
-            size_hint_y: 0.9
-            text: "Place holder"
+        #FileChooserIconView:
+        BoxLayout:
+            size_hint_y: 0.1
+            Button:
+                text: "Status"
+                on_release: root.print_status()
+        FileChooserListView:
+            id: filechooser
+            size_hint_y: 0.8
+            dirselect: True
+            multiselect: True
         BoxLayout:
             orientation: 'horizontal'
             padding: 10, 10, 10, 10
@@ -30,7 +41,14 @@ class ZenFileBrowser(Screen):
     """
     Displays a file browsing screen for ZenPlayer
     """
+    filechooser = ObjectProperty()
+
+
     def __init__(self, sm, playlist, **kwargs):
         self.sm = sm
         self.playlist = playlist
         super(ZenFileBrowser, self).__init__(**kwargs)
+
+    def print_status(self):
+        #TODO: Remove
+        print "fired"
