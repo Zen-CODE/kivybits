514aa6554c07b95049b62aae445db4274f3b1c15
Zen-CODE <zenkey.zencode@gmail.com>
2014-05-30 22:38:30 +0200

    Add removal functionality

--- Scripts/find_duplicates.py
+++ Scripts/find_duplicates.py
@@ -1,24 +1,29 @@
 from os.path import join, isdir
 from os import listdir
+import shutil
 
 
 class FolderSearch(object):
     @staticmethod
     def list_duplicates(folder):
-        FolderSearch._find_case_dups(folder)
+        FolderSearch._remove_case_dups(folder)
 
 
     @staticmethod
-    def _find_case_dups(folder):
+    def _remove_case_dups(folder):
         if isdir(folder):
-            #print "Processing ", folder
+            # Get a list of folder names and lowercase names
             folders = [item for item in listdir(folder)]
             lowers = [item.lower() for item in folders]
-            dups = set([x for x in lowers if lowers.count(x) > 1])
-            for dupe in dups:
-                print "Case duplicate - ", join(folder, dupe)
+
+            # Remove the folder if it's lower case occurs > 1
             for item in folders:
-                FolderSearch._find_case_dups(join(folder, item))
+                if lowers.count(item.lower()) > 1:
+                    lowers.remove(item.lower())
+                    shutil.rmtree(join(folder, item))
+
+            for item in folders:
+                FolderSearch._remove_case_dups(join(folder, item))
 
 if __name__ == "__main__":
     FolderSearch.list_duplicates('/media/Zen320/Zen/Music')
