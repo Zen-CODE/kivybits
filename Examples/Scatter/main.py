c194eb8bf1fd052f906fc3c9f6e1b7941d9aa842
Zen-CODE <zenkey.zencode@gmail.com>
2014-06-20 03:16:00 +0200

    Added some transfrom play-arounds

--- Examples/Scatter/main.py
+++ Examples/Scatter/main.py
@@ -1,29 +1,45 @@
 from kivy.app import App
-from kivy.uix.scatter import Scatter
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 
 Builder.load_string('''
+<DemoScatter>:
+    img: img
+    scat: scat
+    Scatter:
+        id: scat
+        Image:
+            id: img
+            source: "cover.jpg"
+
 <MainWindow>:
     scat1: scat1
     rows: 2
     cols: 2
-    Scatter:
+    DemoScatter:
         id: scat1
-        Image:
-            source: "cover.jpg"
-
-    Scatter:
-        Image:
-            source: "cover.jpg"
-    Scatter:
-        Image:
-            source: "cover.jpg"
-    Scatter:
-        Image:
-            source: "cover.jpg"
+    DemoScatter:
+    DemoScatter:
+    DemoScatter:
 ''')
+
+
+class DemoScatter(RelativeLayout):
+    img = ObjectProperty()
+    scat = ObjectProperty()
+
+    def on_touch_down(self, touch):
+        ret = super(DemoScatter, self).on_touch_down(touch)
+        if self.collide_point(*touch.pos):
+            return True
+        else:
+            # Continue bubbling event
+            return ret
+
+    def apply_transform(self, trans):
+        return self.scat.apply_transform(trans)
 
 
 class MainWindow(GridLayout):
@@ -35,6 +51,7 @@ class MainWindow(GridLayout):
         from kivy.graphics.transformation import Matrix
         mat = Matrix().scale(3, 3, 3)
         self.scat1.apply_transform(mat)
+        print "Transform applied"
 
 
 class TestApp(App):
