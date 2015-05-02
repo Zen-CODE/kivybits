8cafaffd778d2978efbc1ae17c7cdfff886bfe1f
Zen-CODE <zenkey.zencode@gmail.com>
2015-05-02 18:14:58 +0200

    Refactored

--- ZenPlayer/osc/client.py
+++ ZenPlayer/osc/client.py
@@ -2,17 +2,33 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.lib import osc
 from kivy.clock import Clock
-from server import OSCListener
+from server import OSCServer
 
-
-def some_api_callback(message, *args):
-   print("Client: got a message! %s" % message)
 
 kv = '''
 Button:
     text: 'push me!'
     on_press: app.ping()
 '''
+
+
+class OSCClient(object):
+    """ Handles the client side of comms with the OSCServer class. """
+    def __init__(self):
+        super(OSCClient, self).__init__()
+        osc.init()
+        osc_id = osc.listen(ipAddr='127.0.0.1', port=OSCServer.client_port)
+        osc.bind(osc_id, OSCClient.kivy_client, 'kivy_client')
+        Clock.schedule_interval(lambda *x: osc.readQueue(osc_id), 0)
+
+    @staticmethod
+    def kivy_client(message, * args):
+       print("Client: got a message! %s" % message)
+
+    @staticmethod
+    def send_message(args, kwargs):
+        osc.sendMsg('kivy_server', [args, kwargs],
+                    port=OSCServer.server_port)
 
 
 class ServiceApp(App):
@@ -22,18 +38,11 @@ class ServiceApp(App):
         #     service = AndroidService('my pong service', 'running')
         #     service.start('service started')
         #     self.service = service
-
-        osc.init()
-        oscid = osc.listen(ipAddr='127.0.0.1', port=OSCListener.client_port)
-        osc.bind(oscid, some_api_callback, 'kivy_client')
-        Clock.schedule_interval(lambda *x: osc.readQueue(oscid), 0)
-
+        OSCClient()
         return Builder.load_string(kv)
 
     def ping(self):
-        #osc.sendMsg('/some_api', ['ping', ], port=someotherport)
-        osc.sendMsg('kivy_server', ['p1', 'p2', 'p3'],
-                    port=OSCListener.server_port)
+        OSCClient.send_message('args1', 'kwargs1')
 
 
 if __name__ == '__main__':
--- ZenPlayer/osc/server.py
+++ ZenPlayer/osc/server.py
@@ -6,7 +6,7 @@ from time import sleep
 from kivy.lib import osc
 
 
-class OSCListener(object):
+class OSCServer(object):
     """
     Handles messages received of OSC.
     """
@@ -16,20 +16,21 @@ class OSCListener(object):
     def __init__(self):
         """ Create a listener and start managing comms. """
         osc.init()
-        osc_id = osc.listen(ipAddr='127.0.0.1', port=OSCListener.server_port)
-        osc.bind(osc_id, self.kivy_server, 'kivy_server')
-        self.osc_id = osc_id
+        self.osc_id = osc.listen(ipAddr='127.0.0.1', port=OSCServer.server_port)
+        osc.bind(self.osc_id, self.kivy_server, 'kivy_server')
 
     @staticmethod
     def kivy_server(message, *args):
         """
         Handle SoundLoader messages. Message is a list of:
-            [<api_name>, ',s')] + [<args passed in the call>]
+            [<api_name>, ',s')] + [<function>, <args>, <kwargs>]
 
+        Note: args and kwargs are string literals, but this is not yet
+        implemented.
         """
         print("kivy_server : got a message {0}".format(message))
         osc.sendMsg('kivy_client', ['p1', 'p2', 'p3'],
-                    port=OSCListener.client_port)
+                    port=OSCServer.client_port)
 
     def wait(self):
         """ Start the loop where we wait for messages. """
@@ -38,4 +39,4 @@ class OSCListener(object):
             sleep(.1)
 
 if __name__ == '__main__':
-    OSCListener().wait()
+    OSCServer().wait()
