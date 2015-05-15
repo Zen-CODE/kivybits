b1fc7be3dba321348a688f3e8c74f119b5f0f22e
Zen-CODE <zenkey.zencode@gmail.com>
2015-05-15 22:16:33 +0200

    Added support for orientation

--- ZenPlayer/progressbar.py
+++ ZenPlayer/progressbar.py
@@ -3,17 +3,29 @@ __author__ = "Richard Larkin"
 from kivy.uix.progressbar import ProgressBar
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import OptionProperty
 
 Builder.load_string('''
 <-ImageProgressBar>:
     canvas:
+
+        # PushMatrix
+        # Translate:
+        #     xy: self.center_x, self.center_y
+        # Rotate:
+        #     angle: 90 if root.orientation == 'horizontal' else 0
+        #     axis: 0, 0, 1
+        # Translate:
+        #     xy: -self.center_x, -self.center_y
+
         # Color:
         #     rgba: 1, 1, 0, 0.25
         StencilPush
 
         Rectangle:
             pos: self.pos
-            size: self.width * (self.value / self.max), self.height
+            size: self.width * (self.value / self.max) if root.orientation == 'horizontal' else self.width, \
+                  self.height if root.orientation == 'horizontal' else self.height * (self.value / self.max)
         StencilUse
 
         Rectangle:
@@ -23,6 +35,7 @@ Builder.load_string('''
 
         StencilUnUse
         StencilPop
+#        PopMatrix
 
 ''')
 
@@ -31,6 +44,7 @@ class ImageProgressBar(ProgressBar):
     """
     Provides a feature identical yet "prettier" progressbar.
     """
+    orientation = OptionProperty('horizontal', options=['horizontal', 'vertical'])
 
 
 if __name__ == "__main__":
@@ -41,20 +55,21 @@ if __name__ == "__main__":
     class MainScreen(FloatLayout):
         def __init__(self, **kwargs):
             super(MainScreen, self).__init__(**kwargs)
-            self.pb = ImageProgressBar(size_hint=(0.6, 0.2),
+            self.pb = ImageProgressBar(size_hint=(0.5, 0.5),
                                        pos_hint={"center_x": 0.5,
                                                  "center_y": 0.5},
-                                       value=100)
+                                       value=100,
+                                       orientation='vertical')
             self.add_widget(self.pb)
 
             def set_progess(*args, **kwargs):
                 print "setting to {0}".format(kwargs['value'])
                 self.pb.value = kwargs['value']
 
-            for k, val in enumerate(range(100, -10, -10)):
+            for k, val in enumerate(range(100, -1, -1)):
                 print "l, val = {0}, {1}".format(k, val)
-                #Clock.schedule_once(lambda x: partial(set_progess, val), 0.1 * k)
-                Clock.schedule_once(partial(set_progess, value=val), 2 + 0.05 * k)
+                # Clock.schedule_once(lambda x: partial(set_progess, val), 0.1 * k)
+                Clock.schedule_once(partial(set_progess, value=val), 2 + 0.025 * k)
 
 
 
