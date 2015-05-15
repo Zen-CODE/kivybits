80bd3cfc592ccc3e125e4470ffb7a5278c5564f9
Zen-CODE <zenkey.zencode@gmail.com>
2015-05-15 23:01:01 +0200

    Cleanup

--- ZenPlayer/progressbar.py
+++ ZenPlayer/progressbar.py
@@ -3,40 +3,25 @@ __author__ = "Richard Larkin"
 from kivy.uix.progressbar import ProgressBar
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import OptionProperty
+from kivy.properties import StringProperty
 
 Builder.load_string('''
 <-ImageProgressBar>:
     canvas:
-
-        # PushMatrix
-        # Translate:
-        #     xy: self.center_x, self.center_y
-        # Rotate:
-        #     angle: 90 if root.orientation == 'horizontal' else 0
-        #     axis: 0, 0, 1
-        # Translate:
-        #     xy: -self.center_x, -self.center_y
-
-        # Color:
-        #     rgba: 1, 1, 0, 0.25
         StencilPush
 
         Rectangle:
             pos: self.pos
-            size: self.width * (self.value / self.max) if root.orientation == 'horizontal' else self.width, \
-                  self.height if root.orientation == 'horizontal' else self.height * (self.value / self.max)
+            size: self.width * (self.value / self.max), self.height
         StencilUse
 
         Rectangle:
             size: self.size
             pos: self.pos
-            source: "bar1.png"
+            source: root.source
 
         StencilUnUse
         StencilPop
-#        PopMatrix
-
 ''')
 
 
@@ -44,8 +29,7 @@ class ImageProgressBar(ProgressBar):
     """
     Provides a feature identical yet "prettier" progressbar.
     """
-    orientation = OptionProperty('horizontal', options=['horizontal', 'vertical'])
-
+    source = StringProperty('')
 
 if __name__ == "__main__":
     from kivy.app import App
@@ -58,24 +42,20 @@ if __name__ == "__main__":
             self.pb = ImageProgressBar(size_hint=(0.5, 0.5),
                                        pos_hint={"center_x": 0.5,
                                                  "center_y": 0.5},
-                                       value=100,
-                                       orientation='vertical')
+                                       source="bar1.png",
+                                       value=100)
             self.add_widget(self.pb)
 
-            def set_progess(*args, **kwargs):
-                print "setting to {0}".format(kwargs['value'])
-                self.pb.value = kwargs['value']
+            # Animate the value changes
+            def set_progress(*args, **_kwargs):
+                self.pb.value = _kwargs['value']
 
             for k, val in enumerate(range(100, -1, -1)):
-                print "l, val = {0}, {1}".format(k, val)
-                # Clock.schedule_once(lambda x: partial(set_progess, val), 0.1 * k)
-                Clock.schedule_once(partial(set_progess, value=val), 2 + 0.025 * k)
-
-
+                Clock.schedule_once(partial(set_progress, value=val),
+                                    2 + 0.025 * k)
 
     class TestApp(App):
         def build(self):
             return MainScreen()
 
     TestApp().run()
-
