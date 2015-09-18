283161bbfdf93e774ac2c4d02345d9f442f1c785
Zen-CODE <zenkey.zencode@gmail.com>
2015-09-18 14:55:56 +0200

    Revised config example

--- Examples/Config/main.py
+++ Examples/Config/main.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python
 from kivy.app import App
 from kivy.uix.settings import SettingsWithTabbedPanel
-from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.button import Button
 
 
 class MyApp(App):
     def build(self):
-        self.settings_cls=MySettingsWithTabbedPanel
-        return ScreenManager()
+        self.settings_cls = MySettingsWithTabbedPanel
+        self.button = Button(text='Click me to show settings or press F1')
+        self.button.bind(on_release=lambda but: self.open_settings())
+        return self.button
 
-    def build_config(self,config):
-
-        config.setdefaults('Panel Name', {'setting1': True})
+    def build_config(self, config):
+        config.setdefaults('My Button', {'dark_button': True})
 
     def build_settings(self, settings):
+        settings.add_json_panel('My Button', self.config, 'settings.json')
 
-        settings.add_json_panel('Panel Name',
-                             self.config, 'settings.json')
+    def on_config_change(self, config, section, key, value):
+        print("App.on_config_change", config, section, key, value)
 
-    def on_config_change(self, config, section,key,value):
-        print config, section, key, value
+    def close_settings(self, settings):
+        print("App.close_settings", settings)
+        super(MyApp, self).close_settings(settings)
+
 
 class MySettingsWithTabbedPanel(SettingsWithTabbedPanel):
-
-    def on_close(self):
-        print('closed')
+    def _on_close(self):
+        print("MySettingsWithTabbedPanel.on_close")
 
     def on_config_change(self, config, section,key,value):
-        print("Changed!")
-        print config, section, key, value
+        print("MySettingsWithTabbedPanel.on_config_change",config, section,
+              key, value)
 
 MyApp().run()
