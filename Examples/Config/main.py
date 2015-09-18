c5ec79bc9133586d2c51fa7de099cae1c57cccd0
Zen-CODE <zenkey.zencode@gmail.com>
2015-09-18 17:09:21 +0200

    Cleaned up config example

--- Examples/Config/main.py
+++ Examples/Config/main.py
@@ -18,19 +18,26 @@ BoxLayout:
 
 class MyApp(App):
     def build(self):
-        return Builder.load_string(kv)
+        """ Build and return the root widget """
+        root = Builder.load_string(kv)
+        label = root.ids.label
+        label.text = self.config.get('My Label', 'text')
+        label.font_size = float(self.config.get('My Label', 'font_size'))
+        return root
 
     def build_config(self, config):
-        config.setdefaults('My Label', {'text': 'Hello', 'font_size': 12})
+        """ Set the default values for the configs sections. """
+        config.setdefaults('My Label', {'text': 'Hello', 'font_size': 20})
 
     def build_settings(self, settings):
+        """ Add our custom section to the default configuration object. """
         settings.add_json_panel('My Label', self.config, 'settings.json')
 
     def on_config_change(self, config, section, key, value):
+        """ Respond to changes in the configuration. """
         Logger.info("main.py: App.on_config_change: {0}, {1}, {2}, {3}".format(
             config, section, key, value))
 
-        # We respond to changes in the configuration here.
         if section == "My Label":
             if key == "text":
                 self.root.ids.label.text = value
@@ -38,6 +45,7 @@ class MyApp(App):
                 self.root.ids.label.font_size = float(value)
 
     def close_settings(self, settings):
+        """ The settings panel has been closed. """
         Logger.info("main.py: App.close_settings: {0}".format(settings))
         super(MyApp, self).close_settings(settings)
 
@@ -50,6 +58,5 @@ class MySettingsWithTabbedPanel(SettingsWithTabbedPanel):
         Logger.info(
             "main.py: MySettingsWithTabbedPanel.on_config_change: "
             "{0}, {1}, {2}, {3}".format(config, section, key, value))
-
 
 MyApp().run()
