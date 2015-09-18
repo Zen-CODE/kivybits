93ac4fb7257ae2ffc3505a1b760124e6856fe9c3
Zen-CODE <zenkey.zencode@gmail.com>
2015-09-18 16:32:30 +0200

    Added label to example

--- Examples/Config/main.py
+++ Examples/Config/main.py
@@ -1,36 +1,55 @@
 #!/usr/bin/env python
 from kivy.app import App
 from kivy.uix.settings import SettingsWithTabbedPanel
-from kivy.uix.button import Button
+from kivy.logger import Logger
+from kivy.lang import Builder
+
+kv = """
+BoxLayout:
+    orientation: 'vertical'
+    Button:
+        text: 'Click me to show settings or press F1'
+        on_release: app.open_settings()
+    Label:
+        id: label
+        text: 'Hello'
+"""
 
 
 class MyApp(App):
     def build(self):
-        self.settings_cls = MySettingsWithTabbedPanel
-        self.button = Button(text='Click me to show settings or press F1')
-        self.button.bind(on_release=lambda but: self.open_settings())
-        return self.button
+        return Builder.load_string(kv)
 
     def build_config(self, config):
-        config.setdefaults('My Button', {'dark_button': True})
+        config.setdefaults('My Label', {'text': 'Hello', 'font_size': 12})
 
     def build_settings(self, settings):
-        settings.add_json_panel('My Button', self.config, 'settings.json')
+        settings.add_json_panel('My Label', self.config, 'settings.json')
 
     def on_config_change(self, config, section, key, value):
-        print("App.on_config_change", config, section, key, value)
+        Logger.info("main.py: App.on_config_change: {0}, {1}, {2}, {3}".format(
+            config, section, key, value))
+
+        # We respond to changes in the configuration here.
+        if section == "My Label":
+            if key == "text":
+                self.root.ids.label.text = value
+            elif key == 'font_size':
+                self.root.ids.label.font_size = float(value)
 
     def close_settings(self, settings):
-        print("App.close_settings", settings)
+        Logger.info("main.py: App.close_settings: {0}".format(settings))
         super(MyApp, self).close_settings(settings)
 
 
 class MySettingsWithTabbedPanel(SettingsWithTabbedPanel):
-    def _on_close(self):
+    def on_close(self):
         print("MySettingsWithTabbedPanel.on_close")
 
-    def on_config_change(self, config, section,key,value):
-        print("MySettingsWithTabbedPanel.on_config_change",config, section,
-              key, value)
+    def on_config_change(self, config, section, key, value):
+        Logger.info(
+            "main.py: MySettingsWithTabbedPanel.on_config_change: "
+            "{0}, {1}, {2}, {3}".format(config, section, key, value))
+
 
 MyApp().run()
