1f4b207377c15fc70fb1fdba5de619f309e04ec5
Zen-CODE <zenkey.zencode@gmail.com>
2015-09-20 08:31:46 +0200

    Final tweaks to config example

--- Examples/Config/main.py
+++ Examples/Config/main.py
@@ -1,4 +1,15 @@
-#!/usr/bin/env python
+"""
+Config Example
+==============
+
+This file contains a simple example of how the use the Kivy configuration and
+settings modules in a real app. It allows the user to change the caption and
+font_size of the label and stores these changes.
+
+When the user next runs the programs, their changes are restored.
+
+"""
+
 from kivy.app import App
 from kivy.uix.settings import SettingsWithTabbedPanel
 from kivy.logger import Logger
@@ -39,28 +50,40 @@ json = '''
 
 class MyApp(App):
     def build(self):
-        """ Build and return the root widget """
-        root = Builder.load_string(kv)
+        """
+        Build and return the root widget.
+        """
+        # The line below is optional. You could leave it out or use one of the
+        # standard options, such as SettingsWithSidebar, SettingsWithSpinner
+        # etc.
         self.settings_cls = MySettingsWithTabbedPanel
+
         # We apply the saved configuration settings or the defaults
+        root = Builder.load_string(kv)
         label = root.ids.label
         label.text = self.config.get('My Label', 'text')
         label.font_size = float(self.config.get('My Label', 'font_size'))
         return root
 
     def build_config(self, config):
-        """ Set the default values for the configs sections. """
+        """
+        Set the default values for the configs sections.
+        """
         config.setdefaults('My Label', {'text': 'Hello', 'font_size': 20})
 
     def build_settings(self, settings):
-        """ Add our custom section to the default configuration object. """
+        """
+        Add our custom section to the default configuration object.
+        """
         # We use the string defined above for our JSON, but it could also be
         # loaded from a file as follows:
         #     settings.add_json_panel('My Label', self.config, 'settings.json')
         settings.add_json_panel('My Label', self.config, data=json)
 
     def on_config_change(self, config, section, key, value):
-        """ Respond to changes in the configuration. """
+        """
+        Respond to changes in the configuration.
+        """
         Logger.info("main.py: App.on_config_change: {0}, {1}, {2}, {3}".format(
             config, section, key, value))
 
@@ -71,12 +94,22 @@ class MyApp(App):
                 self.root.ids.label.font_size = float(value)
 
     def close_settings(self, settings):
-        """ The settings panel has been closed. """
+        """
+        The settings panel has been closed.
+        """
         Logger.info("main.py: App.close_settings: {0}".format(settings))
         super(MyApp, self).close_settings(settings)
 
 
 class MySettingsWithTabbedPanel(SettingsWithTabbedPanel):
+    """
+    It is not usually necessary to create subclass of a settings panel. There
+    are many built-in types that you can use out of the box
+    (SettingsWithSidebar, SettingsWithSpinner etc.).
+
+    You would only want to create a Settings subclass like this if you want to
+    change the behavior or appearance of an existing Settings class.
+    """
     def on_close(self):
         Logger.info("main.py: MySettingsWithTabbedPanel.on_close")
 
