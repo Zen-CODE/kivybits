d7623e8df57c0c9b69347555365f53ae39aede2b
Zen-CODE <zenkey.zencode@gmail.com>
2016-04-10 09:33:41 +0200

    Added animations

--- PUG/Demo 1/main.py
+++ PUG/Demo 1/main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.animation import Animation
 
 
 class DemoApp(App):
@@ -16,10 +17,10 @@ class DemoApp(App):
                         pos_hint={'center_x': 0.5, 'center_y': 0.8})
         button.bind(on_press=self.button_click)
 
-
         self.image = Image(source='1.png',
                            size_hint=(0.2, 0.2),
-                           pos_hint={'right': 1, 'top': 1})
+                           pos_hint={'right': 1, 'top': 1},
+                           allow_stretch=True)
         float_layout.add_widget(self.image)
 
         float_layout.add_widget(button)
@@ -27,8 +28,9 @@ class DemoApp(App):
 
     def button_click(self, button):
         """ Fired, via binding, when the button is clicked. """
-        anim = An
-
+        anim = Animation(x=0, y=0, duration=1)
+        anim += Animation(size_hint=(1, 1), duration=1, t='in_bounce')
+        anim.start(self.image)
 
 
 if __name__ == "__main__":
