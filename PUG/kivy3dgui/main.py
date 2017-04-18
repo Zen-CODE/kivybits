f00ac6ebe257126e32e758f3a7fdfaff458a9d85
ZenCODE <zenkey.zencode@gmail.com>
2017-04-19 01:53:05 +0200

    3dgui demo done

--- PUG/kivy3dgui/main.py
+++ PUG/kivy3dgui/main.py
@@ -3,6 +3,8 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
+from kivy3dgui.layout3d import Layout3D, Node
+from kivy.animation import Animation
 
 
 class DemoApp(App):
@@ -24,7 +26,29 @@ class DemoApp(App):
     def build(self):
         """ This method returns the root widget of your application. """
         grid = self.get_grid()
-        return grid
+
+        layout3d = Layout3D(canvas_size=(800, 600))
+        node = Node(
+            rotate=(0, 0.3, 1, 0),  # Angle and x, y, z axis of rotation matrix
+            scale=(0.4, 0.4, 0.4),  # x, y, z of scaling matrix
+            translate=(0, 0.0, -100),  # x, y, z of translation matrix
+            effect=True,
+            meshes=("./meshes/2dbox.obj",))
+        node.add_widget(grid)
+        layout3d.add_widget(node)
+
+        ani = self.get_animation()
+        ani.start(node)
+        return layout3d
+
+    @staticmethod
+    def get_animation():
+        """ Return an animation object setting the rotation."""
+        ani = Animation(rotate=(360, 0.3, 1, 0), duration=3) +\
+            Animation(rotate=(0, 0.3, 1, 0), duration=3)
+        ani.repeat = True
+        return ani
 
 if __name__ == "__main__":
     DemoApp().run()
+
