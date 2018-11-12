5a0866e912ad8ad97e101fa128ab11a10ac88aaa
Zen-CODE <zenkey.zencode@gmail.com>
2018-11-12 19:32:17 +0200

    Fleshed out base classes

--- Examples/Services/main.py
+++ Examples/Services/main.py
@@ -1,7 +1,10 @@
+from os import path
 from kivy.app import App
 from kivy.lang.builder import Builder
 from textwrap import dedent
 from kivy.uix.boxlayout import BoxLayout
+from kivy.logger import Logger
+from kivy.utils import platform
 
 kv = dedent('''
         <ServiceUI>:
@@ -22,18 +25,60 @@ kv = dedent('''
 
     ''')
 
-info_text = '''[b]Service Example[/b]\n\n'''\
-    '''In Kivy, Android services are separate, independant processes.'''\
-    '''differs from typical Android services.'''
+class OS(object):
+    """ A convenience class for handling OS specific sunctionality."""
+    def get_user_path():
+        """
+        Return a path that is writable by the current process and can
+        be used to store app data.
+        """
+
+        if platform == "android":
+            from jnius import autoclass, cast
+            PythonActivity = autoclass('org.kivy.android.PythonActivity')
+            Environment = autoclass('android.os.Environment')
+            context = cast('android.content.Context', PythonActivity.mActivity)
+            ret = context.getExternalFilesDir(
+                Environment.getDataDirectory().getAbsolutePath()
+            ).getAbsolutePath()
+        else:
+            root = path.expanduser("~")
+            if platform == "ios":
+                # iOS does not seems to allow for sub-folder creation?
+                # Documents seems to the the place to put it
+                # https://groups.google.com/forum/#!topic/kivy-users/sQXAOecthmE
+                ret = path.join(root, "Documents")
+            else:
+                ret = path.join(root, ".KivyServices")
+        return ret
+
+
 
 class ServiceUI(BoxLayout):
     def __init__(self, **kwargs):
         super(ServiceUI, self).__init__(**kwargs)
-        self.ids.label.text = info_text
+        self.ids.label.text = "[b]Service Example[/b]\n\n"\
+            "In Kivy, Android services are separate, independant processes."\
+            "differs from typical Android services."
 
     def toggle_service(self, number):
         """ Start/Stop the specified server """
-        print("Start/stop service {0}".format(number))
+        # print("Start/stop service {0}".format(number))
+        # # For "android_new", now just "android" toolchain
+
+
+    def _android_service(self):
+        """ Toggle the android service on and off """
+
+        from jnius import autoclass
+        service = autoclass("speedtest.maths.camiweb.com.ServiceCamiapps")
+        mActivity = autoclass(
+            'org.kivy.android.PythonActivity').mActivity
+            
+        param = '{"user_path": "' + OS.get_user_path() + '"}'
+        Logger.info("main.py: service param = {0}".format(param))
+        service.start(mActivity, param)        
+
 
 class ServiceExample(App):
     def build(self):
