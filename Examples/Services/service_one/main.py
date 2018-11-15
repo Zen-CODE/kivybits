61c44950055504f4d2d26c9c428169fefe0b307d
Zen-CODE <zenkey.zencode@gmail.com>
2018-11-15 19:40:16 +0200

    Renamed service

---
+++ Examples/Services/service_one/main.py
@@ -0,0 +1,25 @@
+from time import sleep
+from kivy.logger import Logger
+
+
+class Waiter(object):
+    """
+    This class simply waits for the update to finish and then closes.
+    """
+    def __init__(self):
+        super(Waiter, self).__init__()
+        ''' Hold a reference to the display wrapper class'''
+        self.counter = 0
+
+    def wait(self):
+        """ Start the loop where we wait for messages. """
+        while self.counter < 10:
+            Logger.info("service1/main.py: counter = {0}".format(self.counter))
+            sleep(.5)
+            self.counter += 1 
+        Logger.info("service1/main.py: Exiting")
+
+
+if __name__ == '__main__':
+    Waiter().wait()
+
