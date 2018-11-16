9e05d9b809a8ce3ae987f51ab1f2fb5c602dedea
Richard Larkin <zenkey.zencode@gmail.com>
2018-11-16 14:29:29 +0200

    Added zip and message

--- Examples/Services/service_one/main.py
+++ Examples/Services/service_one/main.py
@@ -1,5 +1,7 @@
 from time import sleep
 from kivy.logger import Logger
+import zipfile
+from os.path import exists
 
 
 class Waiter(object):
@@ -13,11 +15,26 @@ class Waiter(object):
 
     def wait(self):
         """ Start the loop where we wait for messages. """
-        while self.counter < 10:
+        while self.counter < 3:
             Logger.info("service_one/main.py: counter = {0}".format(self.counter))
             sleep(.5)
             self.counter += 1 
-        Logger.info("service_one/main.py: Exiting")
+        
+        Logger.info("service_one/main.py: About to open zip")
+
+    def open_zip():
+        """ Open a standard zip file. """
+        file_name = "service_one/main.zip"
+        if exists(file_name):
+            Logger.info("service_one/main.py: zip found. About to open.")
+            my_zip = zipfile.ZipFile(file_name, "r")
+            Logger.info("service_one/main.py: zip open. contains {0}".format(
+                my_zip.filelist))
+            Logger.info("service_one/main.py: zip examined. Exiting.")
+
+        else:
+            Logger.info("service_one/main.py: zip not found. Exiting.")
+            
 
 
 if __name__ == '__main__':
