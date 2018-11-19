7cf80af483847794628dc7ffeea4ca5b75bb05c2
Richard Larkin <zenkey.zencode@gmail.com>
2018-11-19 15:44:26 +0200

    Added images, extracted kv

--- Examples/Recycleview/camimenurow.py
+++ Examples/Recycleview/camimenurow.py
@@ -8,57 +8,8 @@ from kivy.properties import (ListProperty, BooleanProperty, ObjectProperty,
 from kivy.core.window import Window
 from kivy.uix.label import Label
 
-Builder.load_string('''
-#: import Window kivy.core.window.Window
-#: set gap 0.015 * Window.width
-<CAMIMenuBlock>:
-    canvas:
+Builder.load_file("camimenurow.kv")
 
-        Color:
-            rgba: [0, 0, 0, 0] if not self.pressed else [0, 0.5, 0.5, 0.25]
-        RoundedRectangle:
-            size: self.size
-            pos: self.pos
-    size_hint_x: None
-    width: self.height
-    
-
-<CAMIMenuRow>
-    text: ''
-    callback: None
-    number: 0
-    spacing: 5
-    CAMIMenuBlock:
-        canvas:
-            Color:
-                rgba: 0.5, 1, 0.5, 0.5
-            RoundedRectangle:
-                size: self.size
-                pos: self.pos
-
-        # Houses the number image before the label
-        text: str(root.number)
-        callback: root.callback() if root.callback else None
-
-    BoxLayout:
-        canvas.before:
-            Color:
-                rgba: 0.75, 0.75, 0.1, 0.5
-            RoundedRectangle:
-                size: self.size
-                pos: self.pos
-
-        padding: gap, 0, 0, 0
-        Label:
-            text: root.text + " - " + str(root.number)
-            text_size: self.size
-            halign: 'left'
-            valign: 'center'
-    Widget:
-        # Handles the placement of extra items in the row e.g. trophy/send
-        size_hint_x: None
-        width: 0
-''')
 
 class CAMIMenuBlock(Label):
     """
