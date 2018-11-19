43a4051700e403871df8572374de0791fdc55dd3
Richard Larkin <zenkey.zencode@gmail.com>
2018-11-19 16:20:17 +0200

    Refactored drawing to include number icon

--- Examples/Recycleview/camimenurow.py
+++ Examples/Recycleview/camimenurow.py
@@ -19,23 +19,8 @@ class CAMIMenuBlock(Label):
     callback = ObjectProperty(None)
     """ The callback to fire or press """
 
-    pressed = BooleanProperty(False)
-    """ Indicates whether the block has been tapped or not. """
-
     source = StringProperty('graphics/blank.png')
     """ The graphics to be drawn for the block """
-
-    def on_touch_down(self, touch):
-        """ Respond to the touch down event """
-        if self.collide_point(*touch.pos):
-            self.pressed = True
-        return super(CAMIMenuBlock, self).on_touch_down(touch)
-
-    def on_touch_up(self, touch):
-        """ Respond to the touch up event """
-        if self.pressed:
-            self.pressed = False
-        return super(CAMIMenuBlock, self).on_touch_up(touch)
 
 
 class CAMIMenuRow(BoxLayout):
@@ -47,6 +32,23 @@ class CAMIMenuRow(BoxLayout):
         source: a path to the image
         callback: the callback to be fired on clicking
     """
+
+    pressed = BooleanProperty(False)
+    """ Indicates whether the block has been tapped or not. """
+
+    def on_touch_down(self, touch):
+        """ Respond to the touch down event """
+        if self.collide_point(*touch.pos):
+            self.pressed = True
+            if self.callback:
+                self.callback()
+        return super(CAMIMenuRow, self).on_touch_down(touch)
+
+    def on_touch_up(self, touch):
+        """ Respond to the touch up event """
+        if self.pressed:
+            self.pressed = False
+        return super(CAMIMenuRow, self).on_touch_up(touch)
 
     def on_post_icons(self, widget, icon_list):
         print("camimenurow.py: on_post_icons fired with {0}".format(icon_list))
