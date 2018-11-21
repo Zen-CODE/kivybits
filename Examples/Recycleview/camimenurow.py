da528e11a23bb34ca11b4ef4d571056f4576ac96
Richard Larkin <zenkey.zencode@gmail.com>
2018-11-21 15:13:40 +0200

    Each area now responding to touch with flash

--- Examples/Recycleview/camimenurow.py
+++ Examples/Recycleview/camimenurow.py
@@ -22,6 +22,25 @@ class CAMIMenuBlock(Label):
     source = StringProperty('graphics/blank.png')
     """ The graphics to be drawn for the block """
 
+    pressed = BooleanProperty(False)
+    """ Indicates whether the block has been tapped or not. """
+
+    visible = BooleanProperty(False)
+    """ Indicates whether the widget is displayed or not (has a width)"""
+
+    def on_touch_down(self, touch):
+        """ Respond to the touch down event """
+        if self.collide_point(*touch.pos) and self.visible:
+            self.pressed = True
+
+        return super(CAMIMenuBlock, self).on_touch_down(touch)
+
+    def on_touch_up(self, touch):
+        """ Respond to the touch up event """
+        if self.pressed:
+            self.pressed = False
+        return super(CAMIMenuBlock, self).on_touch_up(touch)
+
 
 class CAMIMenuRow(BoxLayout):
     """
@@ -33,28 +52,12 @@ class CAMIMenuRow(BoxLayout):
         callback: the callback to be fired on clicking
     """
 
-    pressed = BooleanProperty(False)
-    """ Indicates whether the block has been tapped or not. """
-
-    def on_touch_down(self, touch):
-        """ Respond to the touch down event """
-        if self.collide_point(*touch.pos):
-            ids = self.ids
-            for name in ids.keys():
-                if ids[name].collide_point(*touch.pos):
-                    ids[name].callback()
-            # self.pressed =  True
-            # if self.callback:
-            #     self.callback()
-        return super(CAMIMenuRow, self).on_touch_down(touch)
-
-    def on_touch_up(self, touch):
-        """ Respond to the touch up event """
-        if self.pressed:
-            self.pressed = False
-        return super(CAMIMenuRow, self).on_touch_up(touch)
-
     def on_post_icons(self, widget, icon_list):
-        print("camimenurow.py: on_post_icons fired with {0}".format(icon_list))
-
+        # print("camimenurow.py: on_post_icons fired with {0}".format(icon_list))
+        post0, post1 = len(icon_list) > 0, len(icon_list) > 1
+        w_post0, w_post1 = self.ids.mb_post0, self.ids.mb_post1
+        w_post0.visible = post0
+        w_post1.visible = post1
+        w_post0.callback = icon_list[0]['callback'] if w_post0 else None
+        w_post1.callback = icon_list[1]['callback'] if w_post0 else None
 
